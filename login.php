b82282126c29bddcb07bb014c9de13cb19815398
pingson <b6p31079@shonan.bunkyo.ac.jp>
2017-10-17 22:16:37 +0900

    施設一覧　施設名追加

--- login.php
+++ login.php
@@ -1,36 +1,38 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html>
 <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="login.css"/>
+    <meta charset="utf-8" />
     <title></title>
+    <link rel="stylesheet" type="text/css" href="login.css" />
 </head>
 <body>
-<?php
-$id = $_GET["ID"];
-$pw = $_GET["PW"];
-$Error = fales;
-if ($id == "114514" && $pw == "810") {
-    $Error = true;
-} else {
-    $Error = false;
-}
-?>
-<?php if ($Error): ?>
+    <?php
+    $id = $_GET["ID"];
+    $pw = $_GET["PW"];
+    $Error = fales;
+    if($id=="114514" && $pw=="810"){
+        $Error=true;
+    }
+    else{
+        $Error=false;
+    }
+    ?>
+    <?php if($Error): ?>
     <div class="login">
         <?php
-        echo 'ようこそ ', $id, ' 様、画面が変わりますのでしばらくお待ち下さい';
+              echo 'ようこそ ',$id,' 様、画面が変わりますのでしばらくお待ち下さい';
         ?>
     </div>
-<?php else: ?>
+    <?php else: ?>
     <div class="login">
         <h1>IDまたはPWが間違っています</h1>
         <form methot="POST" action="login.html">
             <p>
-                <a href="login.html"><input type="submit" value="戻る" 　>
+                <a href="login.html">
+                    <input type="submit" value="戻る" 　 />
             </p>
         </form>
     </div>
-<?php endif ?>
+    <?php endif ?>
 </body>
 </html>
