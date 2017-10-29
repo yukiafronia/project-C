d858fe005c8db97d33ce9c4c73083c96c78ac5fe
pingson <b6p31079@shonan.bunkyo.ac.jp>
2017-10-29 12:40:34 +0900

    間違えてsakuya2424にしたからmasterに戻す

---
+++ conect_DB.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>テスト</title>
+  </head>
+  <body>
+    <div>
+    <?php
+    $user = 'testuser';
+    $password = 'koke9665';
+    $dbName = 'testdb';
+    $host = 'localhost:8889';
+    $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+
+    try{
+      $pdo = new PDO($dsn, $user, $password);
+      $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      echo "データベース{dbName}に接続しました";
+      $pdo = NULL;
+    }catch(Exception $e){
+      echo '<span class="error">エラーがありました</span><br>';
+      echo $e->getMessage();
+      exit();
+    }
+    ?>
+  </div>
+  </body>
+</html>
