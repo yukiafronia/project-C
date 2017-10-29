da5b37e8a61044ce75ccde80cdc33dd8716740fc
pingson <b6p31079@shonan.bunkyo.ac.jp>
2017-10-29 12:44:56 +0900

    sakuya2424に間違えて同期しちゃったw

---
+++ popup.js
@@ -0,0 +1,19 @@
+﻿function disp() {
+
+    // 「OK」時の処理開始 ＋ 確認ダイアログの表示
+    if (window.confirm('本当にいいんですね？')) {
+
+        location.href = "example_confirm.html"; // example_confirm.html へジャンプ
+
+    }
+    // 「OK」時の処理終了
+
+    // 「キャンセル」時の処理開始
+    else {
+
+        window.alert('キャンセルされました'); // 警告ダイアログを表示
+
+    }
+    // 「キャンセル」時の処理終了
+
+}
