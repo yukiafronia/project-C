84e593ca07d9647e35a1ecd24650f99453cc7663
koke9665 <yuuki9665@gmail.com>
2017-11-13 08:25:56 +0900

    ページのレイアウトの更新

--- javascript/main.js
+++ javascript/main.js
@@ -3,9 +3,50 @@ $(function(){
         opacity : 0
     });
 
-    $(".contents").on('click',function(){
-        alert("click by");
-    });
+$(".bottom").on('click',function(){
+
+      $(".detail").animate({
+              opacity : 0
+          },
+          500
+      );
+        $({deg:0}).animate({deg:360},{
+              duration:1300,
+              progress:function() {
+        				$('.detail').css({
+        					transform:'rotateY(' + this.deg + 'deg)'
+        				});
+        			},
+            },
+            500
+          );
+           setTimeout(function(){
+          window.location.href = "./ShisetsuYoyaku.html";
+        },600);
+});
+
+$(".bottom2").on('click',function(){
+
+      $(".detail").animate({
+              opacity : 0
+          },
+          500
+      );
+        $({deg:0}).animate({deg:360},{
+              duration:1300,
+              progress:function() {
+        				$('.detail').css({
+        					transform:'rotateY(' + this.deg + 'deg)'
+        				});
+        			},
+            },
+            500
+          );
+        setTimeout(function(){
+       window.location.href = "./How_to_use.html";
+     },600);
+});
+
 
 
     $(window).on('load',function(){
@@ -17,15 +58,3 @@ $(function(){
         );
     });
 });
-
-// window.onload = function(){
-//     alert('javascript');
-// }
-
-// $(document).onload( function(){
-//     alert('jQuery');
-// });
-
-// $(window).on('load',function(){
-//     alert('Hello');
-// });
