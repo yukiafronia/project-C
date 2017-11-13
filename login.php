a37cbbd93c63650cf00aaec4f5195a6b3e9bc8d3
koke9665 <yuuki9665@gmail.com>
2017-11-14 00:52:27 +0900

    おまんこ

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+</div>
 <html>
 <head>
     <meta charset="utf-8"/>
@@ -6,6 +7,36 @@
     <link rel="stylesheet" type="text/css" href="login.css"/>
 </head>
 <body>
+  <?php
+  $user = 'testuser';
+  $password = 'koke9665';
+  $dbName = 'testdb';
+  $host = 'localhost:8889';
+  $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+
+  try {
+      $pdo = new PDO($dsn, $user, $password);
+      $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      echo "データベース{dbName}に接続しました";
+      $pdo = NULL;
+
+
+  $sql = "SELECT * FROM user";
+  $stm = $pdo->prepare($sql);
+  $stm->execute();
+  $result = $stm ->fetchAll(PDO::FETCH_ASSOC);
+  echo "otinpoS";
+    foreach($result as $row){
+    echo "<th>", es($result['name']),"</td>";
+      echo "<th>", es($result['password']),"</td>";
+    }
+  } catch (Exception $e) {
+      echo '<span class="error">エラーがありました</span><br>';
+      echo $e->getMessage();
+      exit();
+  }
+  ?>
 <?php
 $id = $_GET["ID"];
 $pw = $_GET["PW"];
@@ -19,7 +50,7 @@ if ($id == "114514" && $pw == "810") {
 <?php if ($Error): ?>
     <div class="login">
         <?php
-        require("conect_DB.php");
+        require_once("conect_DB.php");
         echo 'ようこそ ', $id, ' 様、画面が変わりますのでしばらくお待ち下さい';
         ?>
     </div>
