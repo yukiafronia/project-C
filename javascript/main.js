0abed307094f309dd62ac6099b835ec00909b217
福井 佑希 <b6p31080@shonan-ad.shonan.bunkyo.ac.jp>
2017-11-14 14:14:49 +0900

    いろいろ変更

--- javascript/main.js
+++ javascript/main.js
@@ -1,81 +1,81 @@
 $(function(){
 
-$(".bottom").on('click',function(){
+  $(".bottom").on('click',function(){
 
-      $(".detail").animate({
-              opacity : 0
-          },
-          500
-      );
-        $({deg:0}).animate({deg:360},{
-              duration:1300,
-              progress:function() {
-        				$('.detail').css({
-        					transform:'rotateY(' + this.deg + 'deg)'
-        				});
-        			},
-            },
-            500
-          );
-           setTimeout(function(){
-          window.location.href = "./ShisetsuYoyaku.html";
-        },600);
+    $(".detail").animate({
+      opacity : 0
+    },
+    500
+  );
+  $({deg:0}).animate({deg:360},{
+    duration:1300,
+    progress:function() {
+      $('.detail').css({
+        transform:'rotateY(' + this.deg + 'deg)'
+      });
+    },
+  },
+  500
+);
+setTimeout(function(){
+  window.location.href = "./ShisetsuYoyaku.html";
+},600);
 });
 
 $(".bottom2").on('click',function(){
 
-      $(".detail").animate({
-              opacity : 0
-          },
-          500
-      );
-        $({deg:0}).animate({deg:360},{
-              duration:1300,
-              progress:function() {
-        				$('.detail').css({
-        					transform:'rotateY(' + this.deg + 'deg)'
-        				});
-        			},
-            },
-            500
-          );
-        setTimeout(function(){
-       window.location.href = "./How_to_use.html";
-     },600);
+  $(".detail").animate({
+    opacity : 0
+  },
+  500
+);
+$({deg:0}).animate({deg:360},{
+  duration:1300,
+  progress:function() {
+    $('.detail').css({
+      transform:'rotateY(' + this.deg + 'deg)'
+    });
+  },
+},
+500
+);
+setTimeout(function(){
+  window.location.href = "./How_to_use.html";
+},600);
 
 });
 
 $("#login").on('click',function(){
 
-      $(".detail").animate({
-              opacity : 0
-          },
-          700
-      );
-        $({deg:0}).animate({deg:360},{
-              duration:1300,
-              progress:function() {
-        				$('.detail').css({
-        					transform:'rotateY(' + this.deg + 'deg)'
-        				});
-        			},
-            },
-            500
-          );
-        setTimeout(function(){
-       window.location.href = "./login.html";
-     },600);
+  $(".detail").animate({
+    opacity : 0
+  },
+  700
+);
+$({deg:0}).animate({deg:360},{
+  duration:1300,
+  progress:function() {
+    $('.detail').css({
+      transform:'rotateY(' + this.deg + 'deg)'
+    });
+  },
+},
+500
+);
+setTimeout(function(){
+  window.location.href = "./login.html";
+},600);
 
 });
 
 
 
 
-    $(window).on('load',function(){
-        $(".detail").animate({
-                opacity : 1
-            },
-            500
-        );
-    });
+$(window).on('load',function(){
+  $(".detail").animate({
+    opacity : 1
+  },
+  500
+);
+});
 });
