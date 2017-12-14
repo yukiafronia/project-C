bcd08480eb89e86b52d54e23d68306f50b5bc60b
koke9665 <yuuki9665@gmail.com>
2017-12-15 05:45:17 +0900

    マイページの作成

--- mypage.php
+++ mypage.php
@@ -5,9 +5,17 @@ session_start();
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="mypage.css"/>
     <title></title>
   </head>
   <body>
+    <div class = "name">
+    <?php
+    echo ($_SESSION['name'])."様のマイページ";
+    ?>
+  </div>
+  <h1>お知らせ</h1>
+    <div class = "yoyaku">
     <?php
 
     //予約情報の呼び出し
@@ -24,7 +32,7 @@ session_start();
         //pdoを通して指示
         $dbh = new PDO($dsn, $user, $password);
         //var_dump($dbh);
-        echo 'データベース' . $dbn . 'に接続しました';
+        //echo 'データベース' . $dbn . 'に接続しました<br>';
 
         //SQL文の取り出し
         $sql = "SELECT * FROM `yoyakujoho` WHERE yoyakusya='$yoyakusya'";
@@ -32,17 +40,31 @@ session_start();
         $stm = $dbh->query($sql);
         // var_dump($stm);
         foreach ($stm as $value) {
-        echo '予約日程 : ' . $value['sisetu']. $value['year'] . $value['month'] . $value['day'] . $value['time'] . $value['place'];
+        echo '予約日程 : ' . $value['sisetu']. $value['year'] . $value['month'] . $value['day'] . $value['time'] . $value['place']."にて予約を行いました。<br>";
 
         }
     } catch (PDOException $e) {
         print('Error;' . $e->getMessage());
         die();
     }
-    // 接続を閉じる
-    $dbh = null;
-
 
     ?>
+  </div>
+  <h1>予約の状態</h1>
+  <div class ="yoyaku">
+  <?php
+  //SQL文の取り出し
+  $sql = "SELECT * FROM `yoyakujoho` WHERE yoyakusya='$yoyakusya'";
+
+  $stm = $dbh->query($sql);
+  // var_dump($stm);
+  foreach ($stm as $value) {
+  echo  '場所 : '.$value['sisetu'].' /年 : '. $value['year'] .' /月 : '. $value['month'] .' /日付 : '. $value['day'] .' /時刻 : '. $value['time'] .' /場所 : '. $value['place']."<br>";
+
+  }
+  // 接続を閉じる
+  $dbh = null;
+  ?>
+</div>
   </body>
 </html>
